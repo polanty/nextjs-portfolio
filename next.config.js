16a6a62c8b1cfb1072861e26f48cf7b39263f488
polanty <Tijaniabiola1599@gmail.com>
2025-03-07 16:18:23 -0800

    Unoptimized the images for netlify display

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    unoptimized: true, // Allows Next.js to use standard <img> instead of the Next.js optimization loader
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
