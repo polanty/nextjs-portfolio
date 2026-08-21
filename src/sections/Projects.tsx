c0fd817fff6868f27503a47500c64ef8ef859716
polanty <Tijaniabiola1599@gmail.com>
2026-08-21 21:23:01 +0100

    pulled from my master branch to update local branch

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -7,35 +7,70 @@ import { motion } from "framer-motion";
 function Projects() {
   const projectsData = [
     {
-      image: "/jdsportsreplica.png",
-      projectName: "JD-Sports Clone",
-      projectLink: "https://jdsportsreplica-ecommerce.netlify.app/",
+      image: "/prepwise.png",
+      projectName: "Prepwise",
+      projectLink: "https://www.prepwise.live/",
       projectDescription:
-        "A fully responsive e-commerce website inspired by JD Sports, designed to provide a seamless online shopping experience. This project features dynamic product listings, user authentication, and a shopping cart system. Built with React, Redux, and Firebase, it incorporates modern UI/UX principles to ensure a smooth user journey. The application includes secure payment integration, product filtering, and a wishlist feature, offering a real-world e-commerce experience.",
-      projectTech: ["React", "Context Api", "Firebase", "Typescript", "Stripe"],
+        "PrepWise is an intelligent interview preparation platform that helps job seekers practice and improve their interview skills through AI-driven coaching, real-time speech analysis, and realistic mock interview simulations with a video avatar interviewer.",
+      projectTech: [
+        "Tailwind CSS",
+        "Next.js",
+        "Open AI API",
+        "Smile API",
+        "ai",
+        "jsonwebtoken",
+      ],
       projectExternalLinks: {
-        github: "https://github.com/polanty/jDsportsClone",
-        externalLink: "https://jdsportsreplica-ecommerce.netlify.app/",
+        github: "https://github.com/polanty/Prepwise",
+        externalLink: "https://www.prepwise.live/",
       },
     },
     {
-      image: "/Iphone-screenshot.jpg",
-      projectName: "Apple Website",
-      projectLink: "https://apple-by-polanty.vercel.app/",
+      image: "/Voyage.png",
+      projectName: "Voyage Apartments",
+      projectLink: "https://voyage.apartments/",
       projectDescription:
-        "A GitHub Profile Finder built using Object-Oriented Programming (OOP) principles and the GitHub API to fetch and display user profiles dynamically. This project demonstrates clean architecture, modular design, and API integration, allowing users to search GitHub usernames and view their repositories, followers, and contributions. The UI is intuitive and minimalistic, providing a smooth user experience.",
+        "A scalable, multi-region hotel search and booking web application built with the MERN stack (MongoDB, Express, React, Node.js). PolantyHotels aggregates partnered multinational hotel brands and provides a seamless booking experience for customers while offering comprehensive admin and regional management tools.",
       projectTech: [
-        "Tailwind CSS",
         "React.js",
-        "Three.js",
-        "React Three Fiber",
+        "React Router",
+        "mapbox-gl",
+        "axios",
+        "bycrypt",
+        "jest",
+        "mongoose",
+        "express",
+        "node.js",
+        "MongoDB",
+        "multer",
+        "nodeMailer",
         "React Three Drei",
-        "GSAP",
-        "Vite",
       ],
       projectExternalLinks: {
-        github: "https://github.com/polanty/Apple",
-        externalLink: "https://apple-by-polanty.vercel.app/",
+        github: "https://github.com/polanty/PolantyHotels",
+        externalLink: "https://voyage.apartments/",
+      },
+    },
+    {
+      image: "/EgoBank.png",
+      projectName: "Ego Bank",
+      projectLink: "https://www.egobank.dev/",
+      projectDescription:
+        "A TypeScript Next.js App Router web application that implements a small, fully functional demo bank (user sign-up/sign-in, user profiles, personal/business pages, and money transfers). It’s a frontend-first full-stack demo that uses Firebase for auth/back-end integration and Redux Toolkit (RTK Query) to model API/transfer flows — intended for developers or as a demo product for end users.",
+      projectTech: [
+        "Redux Toolkit",
+        "bootstrap",
+        "firebase",
+        "jspdf",
+        "pdf-lib",
+        "zod",
+        "next",
+        "GSAP",
+        "React-redux",
+      ],
+      projectExternalLinks: {
+        github: "https://github.com/polanty/My-Bank-App",
+        externalLink: "https://www.egobank.dev/",
       },
     },
   ];
@@ -119,7 +154,7 @@ function Projects() {
                 </div>
               </motion.div>
             );
-          }
+          },
         )}
       </div>
     </div>
